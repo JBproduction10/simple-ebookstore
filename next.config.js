ee3f70ac6d3c2e86d947ad596fecd64704bee1e1
Jonathan Bangala <jbangala90@gmail.com>
2024-10-09 14:05:53 +0200

    Make mobile responsive later

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    images: {
+      domains: [
+        "marketplace.canva.com",
+        "d1csarkz8obe9u.cloudfront.net", 
+        "edit.org",
+        "designforwriters.com",
+        "creativereview.imgix.net",
+        "img0-placeit-net.s3-accelerate.amazonaws.com",
+      ],
+    },
+  }
