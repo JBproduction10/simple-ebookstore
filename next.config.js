bac737e2dab0ce4c8e77878b987cf8a04c242754
Jonathan Bangala <jbangala90@gmail.com>
2024-10-09 14:15:02 +0200

    Make mobile responsive later

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     images: {
-      domains: [
-        "marketplace.canva.com",
-        "d1csarkz8obe9u.cloudfront.net", 
-        "edit.org",
-        "designforwriters.com",
-        "creativereview.imgix.net",
-        "img0-placeit-net.s3-accelerate.amazonaws.com",
-      ],
+        domains: [
+            'cdn.imagin.studio',
+            "marketplace.canva.com",
+            "d1csarkz8obe9u.cloudfront.net", 
+            "edit.org",
+            "designforwriters.com",
+            "creativereview.imgix.net",
+            "img0-placeit-net.s3-accelerate.amazonaws.com",
+        ],
     },
-  }
+    typescript:{
+        ignoreBuildErrors: true,
+    }
+}
+
+module.exports = nextConfig
